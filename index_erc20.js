aad81d9d9628b7e42c460e65d30f5615fb08ef41
geby9469 <bjk9406@gmail.com>
2024-03-11 15:37:02 +0900

    implement transfer

--- index_erc20.js
+++ index_erc20.js
@@ -9,7 +9,7 @@ const TOKEN_MINTER = process.env.TOKEN_MINTER;
 // ERC20 token
 const {
   abi,
-} = require("./artifacts/contracts/ERC20_MyToken.sol/ERC20MyToken.json");
+} = require("./artifacts/contracts/ERC20/ERC20_MyToken.sol/ERC20MyToken.json");
 const provider = new ethers.providers.JsonRpcProvider();
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
@@ -38,6 +38,20 @@ app.get("/balance", async (req, res) => {
     const balHex = result._hex;
     const balance = BigNumber.from(balHex).toString();
     console.log(`${TOKEN_MINTER}'s balance = ${balance}`);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/transfer", async (req, res) => {
+  try {
+    // receiver
+    const receiver = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+    const txTransfer = await contract.transfer(receiver, 1000);
+    await txTransfer.wait();
+    console.log(
+      `sender: ${TOKEN_MINTER}\nreceiver: ${receiver}\nsent token: 1000`
+    );
   } catch (error) {
     res.status(500).send(error.message);
   }
