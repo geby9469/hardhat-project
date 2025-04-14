182d81b93acb4584c25604c05417ca4b62ef3255
geby9469 <bjk9406@gmail.com>
2025-04-14 14:12:59 +0900

    fix: function selector not recognized in proxy contract

--- upgradeable-account-abstraction/test/ERC4337/AccountContract.ts
+++ upgradeable-account-abstraction/test/ERC4337/AccountContract.ts
@@ -1,6 +1,6 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import hre, { ethers, upgrades } from 'hardhat';
+import hre, { ethers } from 'hardhat';
 
 describe("AccountContract", function () {
     async function deployAccountContractFixture() {
@@ -20,51 +20,52 @@ describe("AccountContract", function () {
       return { eoa, otherEoa, salt, entryPoint, accountContract, accountContractFactory, token };
     }
 
-    describe("Deployment", function () {
-      it("Check deploy", async function () {
+    describe("EntryPoint", function () {
+      it("Should verify that the Entrypoint contract is deployed", async function () {
         const { entryPoint, accountContract } = await loadFixture(deployAccountContractFixture);
 
         expect(await accountContract.entryPoint()).to.equal(entryPoint);
       });
 
-      it("Check owner", async function () {
-        const { accountContract } = await loadFixture(deployAccountContractFixture);
+      it("Should send 1 ether via EntryPoint's handleOps using a UserOperation", async function () {
+        const {} = await loadFixture(deployAccountContractFixture);
 
-        expect(await accountContract.owner()).to.equal(ethers.ZeroAddress);
+        // * NOTE: Skipped here since it's already implemented in the nestjs-project.
+        // Please refer to the link below as the test code has already been implemented.
+        // For more information, see https://github.com/eth-infinitism/account-abstraction/blob/develop/test/UserOp.ts#L381
       });
     });
 
-    describe("Execution", function () {
-      it("Create AccountContract", async function () {
-        const { eoa, salt, accountContractFactory  } = await loadFixture(deployAccountContractFixture);
+    describe("AccountContract", function () {
+      it("Should ensure the AccountContract owner is the EOA and the Entrypoint matches the deployed one", async function () {
+        const { eoa, salt, entryPoint, accountContractFactory  } = await loadFixture(deployAccountContractFixture);
 
         await accountContractFactory.createAccount(eoa, salt);
-        const accountContract = await accountContractFactory.getAddress(eoa, salt);
+        // * IMPORTANT: If you call getAddress directly, it will return the Factory contract's address,
+        // so you must use getFunction to call it instead.
+        const accountContract = await accountContractFactory.getFunction('getAddress')(eoa, salt);
         const deployedAccountContract = await ethers.getContractAt("AccountContract", accountContract);
 
-        expect(deployedAccountContract.target).to.be.equal(accountContract);
+        expect(entryPoint.target).to.be.equal(await deployedAccountContract.entryPoint());
+        expect(eoa.address).to.be.equal(await deployedAccountContract.owner());
       });
 
-      it("Execute the function of ERC20 balanceOf through AccountContract", async function () {
-        const { eoa, salt, accountContractFactory, token } = await loadFixture(deployAccountContractFixture);
+      it("Should send 1 ether through the AccountContract", async function () {
+        const { eoa, otherEoa, salt, accountContractFactory, token } = await loadFixture(deployAccountContractFixture);
 
-        // Create AccountContract
         await accountContractFactory.createAccount(eoa, salt);
-        const accountContract = await accountContractFactory.getAddress(eoa, salt);
+        const accountContract = await accountContractFactory.getFunction('getAddress')(eoa, salt);
         const deployedAccountContract = await ethers.getContractAt("AccountContract", accountContract);
 
-        // destination smart contract(ERC20 token).
-        const destAddress = await token.getAddress();
+        // Pre-fund the AccountContract with tokens
+        await token.transfer(deployedAccountContract.target, 1);
 
-        // Ether
-        const value = 0;
+        // Set the token contract address
+        // Encode the call data for the ERC20 token transfer function
+        await deployedAccountContract.execute(await token.getAddress(), 0, token.interface.encodeFunctionData('transfer', [otherEoa.address, 1]));
 
-        // Encode the function of ERC20 token.
-        const owner = await eoa.getAddress(); // token owner
-        const callData = token.interface.encodeFunctionData('balanceOf', [owner]);
-
-        const res = await deployedAccountContract.execute(destAddress, value, callData);
-        // TODO: ProviderError: Error: Transaction reverted: function selector was not recognized and there's no fallback function
+        expect(await token.balanceOf(eoa.address)).to.be.equal(9);
+        expect(await token.balanceOf(otherEoa.address)).to.be.equal(1);
       });
     });
 });
